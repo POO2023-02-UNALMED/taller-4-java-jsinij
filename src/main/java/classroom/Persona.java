06e1ceafd0862e37a893ab870275526bf1ee9ab7
jsinij <141966551+jsinij@users.noreply.github.com>
2023-09-01 19:26:45 -0500

    por favor
    estecodigofunciona

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -22,7 +22,7 @@ public class Persona {
     }
     
     public Persona(String nombre){
-    	 this.cedula = 0;
+    	 this.cedula = 1;
          this.nombre = nombre;
          totalPersonas++;
     }
